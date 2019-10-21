4414ce2cd7c97205a64848e1889d2ae2137df798
vinodkumarjadhav <vinodkumarjadhav@gmail.com>
2019-10-21 16:54:28 +0530

    added func2

--- test.c
+++ test.c
@@ -4,12 +4,17 @@ void func1()
 {
 	printf("It is called from %s \n",__func__);
 }
+void func2()
+{
+	printf("It is called from %s \n",__func__);
+}
 
 int main(void)
 {
 	printf("Hey!!! This is just for test to commit\n");
 	//func1 call
 	func1();
+	func2();
 	return 0;
 }
 
