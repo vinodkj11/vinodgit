b6b29104b0aeb7bb148d7e116d094b2cedfff89a
vinodkumarjadhav <vinodkumarjadhav@gmail.com>
2019-10-21 16:34:21 +0530

    added new function

--- test.c
+++ test.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+void func1()
+{
+	printf("It is called from %s \n",__func__);
+}
+
 int main(void)
 {
 	printf("Hey!!! This is just for test to commit\n");
+	//func1 call
+	func1();
 	return 0;
 }
 
